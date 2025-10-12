ee37a05a85547b0807a1bdb8983711eac82086c5
Jmorto1 <yealem082@gmail.com>
2025-10-13 00:03:49 +0300

    tenth update for diployment

--- backend/apps/users/signals.py
+++ backend/apps/users/signals.py
@@ -22,7 +22,7 @@ def create_initial_data(sender, **kwargs):
         # Check if admin exists for this department
         admin = User(
             phone_number="0922222222",
-            email="tempSysAdmin@gmail.com",
+            email="tempAdmin@gmail.com",
             role="admin",
             department=dept,
             name_am="ፅ/ቤት ሀላፊ",
