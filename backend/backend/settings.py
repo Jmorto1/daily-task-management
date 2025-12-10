7da1e92a10bde1681e6105952b61619ec6de72a8
yealem birhanu <yealem082@gmail.com>
2025-12-10 19:27:22 +0300

    neon database commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,7 +98,7 @@ tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': tmpPostgres.path.replace('/', ''),
+        'NAME': tmpPostgres.path.replace('/' ),
         'USER': tmpPostgres.username,
         'PASSWORD': tmpPostgres.password,
         'HOST': tmpPostgres.hostname,
